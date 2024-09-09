93a911e33114314229354c4c044a5ab7ff4a4fa8
Facundo <ramirezfacundo005@gmail.com>
2024-09-09 10:39:16 -0300

    guardado ejercicio clase 4

---
+++ Clase_4/Funciones.py
@@ -0,0 +1,3 @@
+def sumar_2(un_numero, otro_numero): #parametros formales
+    suma = un_numero + otro_numero
+    print(f"La suma es: {suma}")
---
+++ Clase_4/Main.py
@@ -0,0 +1,7 @@
+# import Clase_4.Funciones as PC 
+
+from Funciones import *
+
+
+sumar_2(5, 6)
+
